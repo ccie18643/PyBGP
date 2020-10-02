47ab2d9f4a37c9ad7bb5d87826babca2faf4e978
ccie18643 <ccie18643@gmail.com>
2020-10-02 22:50:42 +0000

    Added better error handling for messages

--- bgp_message.py
+++ bgp_message.py
@@ -8,10 +8,41 @@ KEEPALIVE = 4
 
 HEADER_SIZE = 19
 
+# Error codes
+MESSAGE_HEADER_ERROR = 1
+OPEN_MESSAGE_ERROR = 2
+UPDATE_MESSAGE_ERROR = 3
+HOLD_TIMER_EXPIRED = 4
+FINITE_STATE_MACHINE_ERROR = 5
+CEASE = 6
+
+# Meassage header error subcodes
+CONNECTION_NOT_SYNCHRONISED = 1
+BAD_MESSAGE_LENGTH = 2
+BAD_MESSAGE_TYPE = 3
+
+# OPEN message error subcodes
+UNSUPPORTED_VERSION_NUMBER = 1
+BAD_PEER_AS = 2
+BAD_BGP_IDENTIFIER = 3
+UNSUPPORTED_OPTIONAL_PARAMETER = 4
+UNACEPTABLE_HOLD_TIME = 6
+
+# UPDATE message error subcodes
+MALFORMED_ATTRIBUTE_LIST = 1
+UNRECOGNIZED_WELL_KNOWN_ATTRIBUTE = 2
+MISSING_WELL_KNOWN_ATTRIBUTE = 3
+ATTRIBUTE_FLAGS_ERROR = 4
+ATTRIBUTE_LENGTH_WRROR = 5
+INVALID_ORIGIN_ATTRIBUTE = 6
+INVALID_NEXT_HOP_ATTRIBUTE = 8
+OPTIONAL_ATTRIBUTE_ERROR = 9
+INVALID_NETWORK_FIELD = 10
+MALFORMED_AS_PATH = 11
 
 class DecodeMessage():
-    def __init__(self, data):
-      
+    def __init__(self, data, asn=0, bgp_id="0.0.0.0"):
+
         self.data_length_error = False
         self.data_length_expected = 19
         self.data_length_received = len(data)
@@ -28,23 +59,23 @@ class DecodeMessage():
         # Validate Marker field
         for m in struct.iter_unpack("!B", data[:16]):
             if m[0] != 0xff:
-                self.message_error_code = 1
-                self.message_error_subcode = 1
+                self.message_error_code = MESSAGE_HEADER_ERROR
+                self.message_error_subcode = CONNECTION_NOT_SYNCHRONISED
                 return
 
         self.length, self.type = struct.unpack("!HB", data[16:19])
 
         # Validate Length field
         if self.length < 19 or self.length > 4096:
-            self.message_error_code = 1
-            self.message_error_subcode = 2
+            self.message_error_code = MESSAGE_HEADER_ERROR
+            self.message_error_subcode = BAD_MESSAGE_LENGTH
             self.message_error_data = struct.pack("!H", self.length)
             return
 
         # Validate Type field
         if self.type not in {OPEN, UPDATE, NOTIFICATION, KEEPALIVE}:
-            self.message_error_code = 1
-            self.message_error_subcode = 3
+            self.message_error_code = MESSAGE_HEADER_ERROR
+            self.message_error_subcode = BAD_MESSAGE_TYPE
             self.message_error_data = struct.pack("!B", self.type)
             return
 
@@ -58,13 +89,33 @@ class DecodeMessage():
 
             # Validate Length field
             if self.length < 19 + 10:
-                self.message_error_code = 1
-                self.message_error_subcode = 2
+                self.message_error_code = MESSAGE_HEADER_ERROR
+                self.message_error_subcode = BAD_MESSAGE_LENGTH
                 self.message_error_data = struct.pack("!H", self.length)
                 return
 
             self.version, self.asn, self.hold_time, self.bgp_id, self.opt_len = struct.unpack("!BHHIB", data[19:29])
+            self.bgp_id = socket.inet_ntoa(struct.pack('!L', self.bgp_id))
             opt_param = data[29:self.length]
+
+            if self.version != 4:
+                self.message_error_code = OPEN_MESSAGE_ERROR
+                self.message_error_subcode = UNSUPPORTED_VERSION_NUMBER
+
+            if self.asn != asn:
+                self.message_error_code = OPEN_MESSAGE_ERROR
+                self.message_error_subcode = BAD_PEER_AS
+
+            ### Need to check for valid unicast IP
+            if self.bgp_id == bgp_id:
+                self.message_error_code = OPEN_MESSAGE_ERROR
+                self.message_error_subcode = BAD_BGP_IDENTIFIER
+
+            ### Need to add check for supported optional parameters
+
+            if self.hold_time in {1, 2}:
+                self.message_error_code = OPEN_MESSAGE_ERROR
+                self.message_error_subcode = UNACCEPTABLE_HOLD_TIME
 
         if self.type == UPDATE:
             pass
