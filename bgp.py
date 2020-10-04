0ffa4c47dc637aa87fbcd29deec80f0efe75351c
ccie18643 <ccie18643@gmail.com>
2020-10-04 01:25:13 +0000

    Got rid of threads and switched back to asyncio for multiple connections

--- bgp.py
+++ bgp.py
@@ -3,7 +3,6 @@
 import sys
 import asyncio
 import loguru
-import threading
 
 import bgp_message
 from bgp_event import BgpEvent
@@ -22,10 +21,13 @@ async def bgp_session_coroutine(local_id, local_asn, local_hold_time, peer_ip, p
         if session.state == "Idle":
             session.event_queue.append(BgpEvent("Event 3: AutomaticStart"))
 
-def bgp_session_thread(local_id, local_asn, local_hold_time, peer_ip, peer_asn):
-    """ Thread for the BGP session """
+async def run_sessions():
+    asyncio.create_task(bgp_session_coroutine("192.168.9.201", 65000, 180, "192.168.9.202", 65000))
+    asyncio.create_task(bgp_session_coroutine("192.168.9.201", 65000, 180, "192.168.9.203", 65000))
+    asyncio.create_task(bgp_session_coroutine("192.168.9.201", 65000, 180, "192.168.9.204", 65000))
 
-    asyncio.run(bgp_session_coroutine(local_id, local_asn, local_hold_time, peer_ip, peer_asn))
+    while True:
+        await asyncio.sleep(1)
 
 
 def main():
@@ -33,11 +35,7 @@ def main():
     loguru.logger.add(sys.stdout, colorize=True, level="DEBUG", format=f"<green>{{time:YY-MM-DD HH:mm:ss}}</green> <level>| {{level:7}} "
             + f"|</level> <level>{{extra[peer_ip]:15}} | <normal><cyan>{{function:33}}</cyan></normal> | {{extra[state]:11}} | {{message}}</level>")
 
-    #threading.Thread(target=bgp_session_thread, args=("192.168.9.201", 65000, 180, "192.168.9.202", 65000)).start()
-    #threading.Thread(target=bgp_session_thread, args=("192.168.9.201", 65000, 180, "192.168.9.20", 65000)).start()
-    threading.Thread(target=bgp_session_thread, args=("192.168.9.201", 65000, 180, "222.222.222.222", 65000)).start()
-    #threading.Thread(target=bgp_session_thread, args=("192.168.9.201", 65000, 180, "192.168.9.203", 65000)).start()
-    #threading.Thread(target=bgp_session_thread, args=("192.168.9.201", 65000, 180, "192.168.9.204", 65000)).start()
+    asyncio.run(run_sessions())
 
 
 if __name__ == "__main__":
